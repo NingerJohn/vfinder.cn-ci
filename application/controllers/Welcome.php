c886dbe432f7f8276ae0baaf87aa5593dab1c8a1
NingerJohn <ningerjohn@163.com>
2015-09-02 08:34:54 +0800

    add index ui file

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -18,20 +18,19 @@ class Welcome extends MY_Controller {
 	}
 
 	/**
-	 * 前端首页
-	 * 显示各种分类的数据
+	 * 
+	 * 方法功能说明
 	 * 
 	 * @author 作者 NingerJohn
-	 * @ctime 创建时间 2015年8月29日17:57:48
 	 * @modifiers 修改人：NingerJohn; mike; jessica
 	 * 
 	 */
 	public function index()
 	{
 		// $this->load->view('welcome_message');
-		$this->welcome_m->()
+		$this->welcome_m->test()；
 		$data = array('num'=>'1111','title'=>'title test');
-		$this->load->view('Welcome/body', $data); // 调用body模板并赋值
+		$this->load->view('Welcome/body', $data); // 调用body视图模板并赋值
 		// echo $res;
 	}
 }
